28df72b2471d92e46bf9c619eb18c3e8db12deaf
jttmtv <52265704+jttmtv@users.noreply.github.com>
2020-12-07 00:00:42 +0800

    Update Matrix.hpp

--- Matrix.hpp
+++ Matrix.hpp
@@ -4,44 +4,47 @@
 #include <initializer_list>
 #include <iostream>
 
+
 class Matrix
 {
 
 private:
-    bool __flag;     //mark whether the memory is belong to the object itself
-    char *__buff;    //buffer
-    float *__data;   //data
-    size_t __rows;   //row
-    size_t __cols;   //col
-    int *__refcount; //reference times
-    bool check() const;
+	bool __flag;             //mark whether the memory is belong to the object itself 
+	char* __buff;           //buffer
+	float* __data;         //data
+	size_t __rows;        //row
+	size_t __cols;       //col
+	int* __refcount;    //reference times
+	bool check() const;
+	friend void memset(Matrix& m, const size_t rows, const size_t cols, const float data);
+	friend void memcpy(Matrix& m1, const Matrix& m2);
 
 public:
-    Matrix();
-    Matrix(const Matrix &m);           //copy constructor
-    Matrix(const Matrix &&m) noexcept; //move constructor
-    Matrix(const size_t rows, const size_t cols, const float data = 0);
-    Matrix(const std::initializer_list<std::initializer_list<float>> &list);
-    ~Matrix();
+	Matrix();
+	Matrix(const Matrix& m); //copy constructor
+	Matrix(const Matrix&& m) noexcept;//move constructor
+	Matrix(const size_t rows, const size_t cols, const float data = 0);
+	Matrix(const std::initializer_list<std::initializer_list<float>>& list);
+	~Matrix();
 
-    Matrix &operator=(const Matrix &m);
-    size_t cols() const;
-    size_t rows() const;
-    void set(const size_t rows, const size_t cols, const float data = 0);
+	Matrix& operator=(const Matrix& m);
+	const size_t cols() const;
+	const size_t rows() const;
+	void set(const size_t rows, const size_t cols, const float data = 0);
 
-    float *operator[](const size_t i);
-    const float *operator[](const size_t i) const; //provide access to the const vriabal
+	float* operator[](const size_t i);
+	const float* operator[](const size_t i) const; //provide access to the const vriabal
 
-    friend std::ostream &operator<<(std::ostream &os, Matrix &&m);
-    friend std::ostream &operator<<(std::ostream &os, Matrix &m);
-    friend std::istream &operator>>(std::istream &is, Matrix &m);
+	friend std::ostream& operator<<(std::ostream& os, Matrix&& m);
+	friend std::ostream& operator<<(std::ostream& os, Matrix& m);
+	friend std::istream& operator>>(std::istream& is, Matrix& m);
 
-    friend bool operator==(const Matrix &m1, const Matrix &m2);
-    friend bool operator!=(const Matrix &m1, const Matrix &m2);
+	friend bool operator==(const Matrix& m1, const Matrix& m2);
+	friend bool operator!=(const Matrix& m1, const Matrix& m2) { return !operator==(m1, m2); }
 
-    friend Matrix operator*(const Matrix &m1, const Matrix &m2);
-    friend Matrix operator*(const float n, const Matrix &m);
-    friend Matrix operator*(const Matrix &m, const float n) { return n * m; }
+	friend Matrix operator*(const Matrix& m1, const Matrix& m2);
+	friend Matrix operator*(const float n, const Matrix& m);
+	friend Matrix operator*(const Matrix& m, const float n) { return n * m; }
 };
 
 #endif
